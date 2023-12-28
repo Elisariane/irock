aa97702cbba49d19048fd57f6e763cebe9215ee9
Elisariane <elisarianeBS@gmail.com>
2023-12-28 08:42:03 -0300

    :sparkles: feat: Add função ao tocar na rocha

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 function sayHi(){
-    alert('Hello, eu sou a rocha de estimação.')
+    alert('Hello, eu sou a rocha de estimação.');
+}
+
+function touchRock() {
+    let userName = prompt("Qual o seu nome?");
+
+    if (userName) {
+        alert(`Prazer em conhecer você, ${userName}!`);
+        document.getElementById("rockImg").src = "img/rock_happy.png";
+    }
 }
